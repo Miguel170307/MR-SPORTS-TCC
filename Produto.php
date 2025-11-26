5455890e7cd883acd670f132c1c53d5714af6ccf
Miguel Teixeira <147268449+Miguel170307@users.noreply.github.com>
2025-11-25 21:52:30 -0300

    Produto.php

---
+++ Produto.php
@@ -0,0 +1,32 @@
+<?php
+class Produto {
+    private $conn;
+    private $table_name = "produtos";
+
+    public $id;
+    public $nome;
+    public $preco;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function read() {
+        $query = "SELECT * FROM " . $this->table_name;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function readOne() {
+        $query = "SELECT * FROM " . $this->table_name . " WHERE id = ? LIMIT 0,1";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->id);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        $this->nome = $row['nome'];
+        $this->preco = $row['preco'];
+    }
+}
+?>
